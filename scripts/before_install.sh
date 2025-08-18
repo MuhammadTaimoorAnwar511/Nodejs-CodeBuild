ffdd4607e43c78671009679722ccf3020015e1b3
MuhammadTaimoorAnwar511 <anwartaimoor511@gmail.com>
2025-08-18 19:13:10 +0500

    ok

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
+# scripts/before_install.sh
+
 # ensure app folder exists
 mkdir -p /home/ubuntu/node-app
+
+# fix ownership and permissions so ubuntu can run scripts and modify files
+chown -R ubuntu:ubuntu /home/ubuntu/node-app
+chmod -R 755 /home/ubuntu/node-app
