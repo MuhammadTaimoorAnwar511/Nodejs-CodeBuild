8ab9e9825fe3e41e2b9d7a4d91834fe72542c21c
MuhammadTaimoorAnwar511 <anwartaimoor511@gmail.com>
2025-08-19 15:37:44 +0500

    option 1

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -7,11 +7,6 @@ rm -rf node_modules package-lock.json
 # install dependencies
 npm install --legacy-peer-deps
 
-# install PM2 globally (as root)
-if ! command -v pm2 >/dev/null; then
-    npm install -g pm2
-fi
-
 # start app with PM2
 pm2 start "npm start" --name node-app
 pm2 save
