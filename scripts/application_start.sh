96eb0bb35f4b0825c5cb56248bf561c15250cb28
MuhammadTaimoorAnwar511 <anwartaimoor511@gmail.com>
2025-08-19 15:55:41 +0500

    ubunut

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+
 cd /home/ubuntu/node-app
 
 # clean old modules (optional)
--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -3,8 +3,11 @@
 # ensure app folder exists
 mkdir -p /home/ubuntu/node-app
 
-# give full permissions (optional, but convenient for root)
+# ensure ubuntu owns everything (useful if pipeline copied files as root)
+chown -R ubuntu:ubuntu /home/ubuntu/node-app
+
+# set permissions
 chmod -R 755 /home/ubuntu/node-app
 
-# make sure the other scripts are executable
+# make sure scripts are executable
 chmod +x /home/ubuntu/node-app/scripts/*.sh
